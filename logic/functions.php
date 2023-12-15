944131cab45ffb0fa667f8496c22bd76edd866ce
aldidaim21 <aldidiamfauzan15@gmail.com>
2023-12-16 00:04:56 +0700

    cek koneksi

---
+++ logic/functions.php
@@ -0,0 +1,8 @@
+<?php
+$konek = mysqli_connect('localhost', 'root', '', 'section4');
+// Check connection
+if (mysqli_connect_errno()) {
+	echo "Koneksi database gagal : " . mysqli_connect_error();
+} else {
+	echo "koneksi berhasil";
+}
