1a2a811041a591f117d4b7b91aa370fc4ebd4dda
aldidaim21 <aldidiamfauzan15@gmail.com>
2023-12-27 12:40:56 +0700

    aldi daim

--- offer_task/oft.php
+++ offer_task/oft.php
@@ -11,75 +11,73 @@ $offers = query("SELECT * FROM offer_task");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Offer Task</title>
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <!-- Your CSS file (if any) -->
+    <!--boostrap--->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <!--css--->
     <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/style3.css">
+
 </head>
 
 <body>
     <!-- Your Navbar Code -->
-    <nav class="navbar navbar-dark bg-dark fixed-top">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Automobile.ID</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container">
+
+
+            <a class="navbar-brand" href="../index.php">Automobile<span>.ID</span></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="offcanvas offcanvas-end text-bg-dark" tabindex="5" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-                <div class="offcanvas-header">
-                    <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Automobile.ID</h5>
-                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                </div>
-                <div class="offcanvas-body">
-                    <ul class="navbar-nav flex-grow-1 pe-1">
-                        <li class="nav-item">
-                            <a class="nav-link" href="../index.php">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="../task_catalog/task_catalog.php">Task Catalog</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="../service_catalog/service.php">Service Catalog</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" href="oft.php">Offer Task</a>
-                        </li>
-                    </ul>
-                </div>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <div class="navbar-nav">
+                        <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
+                        <a class="nav-link" href="../task_catalog/task_catalog.php">Task Catalog</a>
+                        <a class="nav-link" href="../service_catalog/service.php">Service Catalog</a>
+                        <a class="nav-link" href="oft.php">Offer Task</a>
+                    </div>
+                </ul>
             </div>
         </div>
     </nav>
 
-    <h2>Offer Task List</h2>
-    <div class="container mt-5">
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Offer ID</th>
-                    <th>Task Catalog ID</th>
-                    <th>Task Price</th>
-                    <th>Timestamp</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($offers as $row) : ?>
+    <div class="container all">
+
+        <div class="d-flex justify-content-between align-items-center mb-4 big">
+            <h2>Offer Task List</h2>
+            <a class="btn btn-primary" href="tambah_oft.php">TAMBAH DATA</a>
+        </div>
+        <div class="container mt-5">
+            <table class="table table-bordered">
+                <thead>
                     <tr>
-                        <td><?= $row["id"]; ?></td>
-                        <td><?= $row["offer_id"]; ?></td>
-                        <td><?= $row["task_catalog_id"]; ?></td>
-                        <td><?= $row["task_price"]; ?></td>
-                        <td><?= $row["insert_ts"]; ?></td>
-                        <td>
-                            <a href="update_oft.php?id=<?= $row["id"]; ?>" class="btn btn-primary">UPDATE</a>
-                            <a href="delete_oft.php?id=<?= $row["id"]; ?>" class="btn btn-danger" role="button" aria-pressed="true" onclick="return confirm('Are you sure?')">Delete</a>
-                        </td>
+                        <th>ID</th>
+                        <th>Offer ID</th>
+                        <th>Task Catalog ID</th>
+                        <th>Task Price</th>
+                        <th>Timestamp</th>
+                        <th>Action</th>
                     </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    <?php foreach ($offers as $row) : ?>
+                        <tr>
+                            <td><?= $row["id"]; ?></td>
+                            <td><?= $row["offer_id"]; ?></td>
+                            <td><?= $row["task_catalog_id"]; ?></td>
+                            <td><?= $row["task_price"]; ?></td>
+                            <td><?= $row["insert_ts"]; ?></td>
+                            <td>
+                                <a href="update_oft.php?id=<?= $row["id"]; ?>" class="btn btn-primary">UPDATE</a>
+                                <a href="delete_oft.php?id=<?= $row["id"]; ?>" class="btn btn-danger" role="button" aria-pressed="true" onclick="return confirm('Are you sure?')">Delete</a>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
     </div>
-    <a class="btn" href="tambah_oft.php">TAMBAH DATA</a>
 
     <!-- Bootstrap JS and Popper.js -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
--- service_catalog/service.php
+++ service_catalog/service.php
@@ -13,73 +13,87 @@ $services = query("SELECT * FROM service_catalog");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Service Catalog </title>
     <!--boostrap--->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <!--css--->
     <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/style3.css">
 
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Automobile</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container">
+
+
+            <a class="navbar-brand" href="../index.php">Automobile<span>.ID</span></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-
-                <div class="navbar-nav">
-                    <a class="nav-link" aria-current="page" href="../index.php">Home</a>
-                    <a class="nav-link" href="../task_catalog/task_catalog.php">Task Catalog</a>
-                    <a class="nav-link active" href="service.php">Service Catalog</a>
-                    <a class="nav-link" href="../offer_task/oft.php">Offer Task</a>
-                </div>
-
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <div class="navbar-nav">
+                        <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
+                        <a class="nav-link" href="../task_catalog/task_catalog.php">Task Catalog</a>
+                        <a class="nav-link" href="service.php">Service Catalog</a>
+                        <a class="nav-link" href="../offer_task/oft.php">Offer Task</a>
+                    </div>
+                </ul>
             </div>
         </div>
     </nav>
-    <h2>Service List</h2>
-    <div class="container mt-5">
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Service Name</th>
-                    <th>Description</th>
-                    <th>Service Discount</th>
-                    <th>Is Active</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($services as $row) : ?>
-                    <?php
-                    if ($row["is_active"] == 1) {
-                        $hasil = "Servis Aktif";
-                    } else {
-                        $hasil = "Servis Tidak Aktif";
-                    }
-                    ?>
-                    <tr>
-                        <td><?= $row["id"]; ?></td>
-                        <td><?= $row["service_name"]; ?></td>
-                        <td><?= $row["description"]; ?></td>
-                        <td><?= $row["service_discount"]; ?></td>
-                        <td><?= $hasil; ?></td>
-                        <td>
-                            <a href="update_service.php?id=<?= $row["id"]; ?>" class="btn btn-primary">UPDATE</a>
-                            <a href="delete_service.php?id=<?= $row["id"]; ?>" class="btn btn-danger" role="button" aria-pressed="true" onclick="return confirm('yakin');">Delete</a>
-                            <!-- Tambahkan aksi lainnya sesuai kebutuhan -->
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
 
+
+
+
+    <div class="container all">
+
+        <div class="d-flex justify-content-between align-items-center mb-4 big">
+            <h2>Service List</h2>
+            <a class="btn btn-primary" href="tambah_service.php">TAMBAH DATA</a>
+        </div>
+
+        <div class="container mt-5">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Service Name</th>
+                        <th>Description</th>
+                        <th>Service Discount</th>
+                        <th>Is Active</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($services as $row) : ?>
+                        <?php
+                        if ($row["is_active"] == 1) {
+                            $hasil = "Servis Aktif";
+                        } else {
+                            $hasil = "Servis Tidak Aktif";
+                        }
+                        ?>
+                        <tr>
+                            <td><?= $row["id"]; ?></td>
+                            <td><?= $row["service_name"]; ?></td>
+                            <td><?= $row["description"]; ?></td>
+                            <td><?= $row["service_discount"]; ?></td>
+                            <td><?= $hasil; ?></td>
+                            <td>
+                                <a href="update_service.php?id=<?= $row["id"]; ?>" class="btn btn-primary">UPDATE</a>
+                                <a href="delete_service.php?id=<?= $row["id"]; ?>" class="btn btn-danger" role="button" aria-pressed="true" onclick="return confirm('yakin');">Delete</a>
+                                <!-- Tambahkan aksi lainnya sesuai kebutuhan -->
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
     </div>
-    <a class="btn" href="tambah_service.php">TAMBAH DATA</a>
+
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>>
 
 
 </html>
