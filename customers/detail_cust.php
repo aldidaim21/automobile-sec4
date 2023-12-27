5b5eeaf57bd421804fe8a94414b3af0bf784ba7c
gabriellaaan <gabriellaaan15@gmail.com>
2023-12-27 12:53:07 +0700

    details

--- customers/detail_cust.php
+++ customers/detail_cust.php
@@ -14,24 +14,42 @@ $cust = query("SELECT * FROM customer WHERE id = $id")[0];
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Detail <?php echo $cust["first_name"] ?></title>
+
+    <!-- boostrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- css -->
+    <link rel="stylesheet" href="css/style3.css">
+
 </head>
 
 <body>
 
-    <div class="container mt-5">
-        <!-- Nama sebagai h1 -->
-        <h1 class="mb-4" style="text-transform: uppercase;"><?php echo $cust["first_name"] ?><?php echo $cust["last_name"] ?></h1>
-        <!-- Biodata menggunakan kombinasi h2, p, dan Bootstrap classes -->
-        <h2>ID: <span class="text-muted"><?php echo $cust["id"] ?></span></h2>
-        <h2>Company: <span class="text-muted"><?php echo $cust["company_name"] ?></span></h2>
-        <p><strong>Address:</strong> <?php echo $cust["address"] ?></p>
-        <p><strong>Mobile:</strong> <?php echo $cust["mobile"] ?></p>
-        <p><strong>Email:</strong> <?php echo $cust["email"] ?></p>
-        <p><strong>Deskripsi:</strong> <?php echo $cust["detail"] ?></p>
     </div>
+    </div>
+    <div class="container mt-5">
+        <div class="card text-bg-dark mb-3">
+            <div class="card-header ">
+                <strong> Detail Customer </strong>
+            </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+            <div class="card-body">
+                <!-- Nama sebagai h1 -->
+                <h1 class="card-title mb-4" style="text-transform: uppercase;"><?php echo $cust["first_name"] ?><?php echo $cust["last_name"] ?></h1>
+
+                <!-- Biodata menggunakan kombinasi h2, p, dan Bootstrap classes -->
+                <h2>ID: <span class="text-muted"><?php echo $cust["id"] ?></span></h2>
+                <h2>Company: <span class="text-muted"><?php echo $cust["company_name"] ?></span></h2>
+                <br>
+                <p><strong>Address:</strong> <?php echo $cust["address"] ?></p>
+                <p><strong>Mobile:</strong> <?php echo $cust["mobile"] ?></p>
+                <p><strong>Email:</strong> <?php echo $cust["email"] ?></p>
+                <p><strong>Deskripsi:</strong> <?php echo $cust["detail"] ?></p>
+
+                <br> <a href="../index.php" class="btn btn-dark">Back to Home Page</a>
+            </div>
+
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
--- offer_services/detailofs.php
+++ offer_services/detailofs.php
@@ -22,20 +22,30 @@ $users1 = query("SELECT * FROM customer WHERE id =$usd  ")[0];
 
 <body>
     <div class="container mt-5">
-        <h1 class="mb-4">Detail Offer Services</h1>
-        <h2>ID: <span class="text-muted"><?= $offer["id"]; ?></span></h2>
-        <h2>Customer ID: <span class="text-muted"><?= $offer["customer_id"]; ?></span></h2>
-        <h2>Customer Name <span class="text-muted"><?= $users1["first_name"]; ?></span></h2>
-        <h2>Contact ID: <span class="text-muted"><?= $offer["contact_id"]; ?></span></h2>
-        <h2>Offer Description: <span class="text-muted"><?= $offer["offer_description"]; ?></span></h2>
-        <h2>Service Catalog ID: <span class="text-muted"><?= $offer["service_catalog_id"]; ?></span></h2>
-        <h2>Service Discount: <span class="text-muted"><?= $offer["service_discount"]; ?></span></h2>
-        <h2>Offer Price: <span class="text-muted"><?= $offer["offer_price"]; ?></span></h2>
-        <h2>Waktu Pembuatan Data: <span class="text-muted"><?= $offer["insert_ts"]; ?></span></h2>
-    </div>
+        <div class="card text mb-3">
+            <div class="card-header ">
+                <strong> Detail Offer Service </strong>
+            </div>
 
-    <!-- Bootstrap JS and Popper.js -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+            <div class="card-body">
+                <h3>ID: <span class="text-muted"><?= $offer["id"]; ?></span></h3>
+                <h3>Customer ID: <span class="text-muted"><?= $offer["customer_id"]; ?></span></h3>
+                <h3>Customer Name: <span class="text-muted"><?= $users1["first_name"]; ?></span></h3>
+                <br>
+                <p>Contact ID: <span class="text-muted"><?= $offer["contact_id"]; ?></span></p>
+                <p>Offer Description: <span class="text-muted"><?= $offer["offer_description"]; ?></span></p>
+                <p>Service Catalog ID: <span class="text-muted"><?= $offer["service_catalog_id"]; ?></span></p>
+                <p>Service Discount: <span class="text-muted"><?= $offer["service_discount"]; ?></span></p>
+                <p>Offer Price: <span class="text-muted"><?= $offer["offer_price"]; ?></span></p>
+                <p>Waktu Pembuatan Data: <span class="text-muted"><?= $offer["insert_ts"]; ?></span></p>
+
+                <br> <a href="../index.php" class="btn btn-dark">Back to Home Page</a>
+            </div>
+
+        </div>
+
+        <!-- Bootstrap JS and Popper.js -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
 </html>
